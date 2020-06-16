c156b6bd95e9d978a811413d26e957ec30043f5a
Soham Patel <54907192+patelsoham@users.noreply.github.com>
2020-06-16 18:46:34 -0500

    Added time submission to easily determine late penalty and made more robust

--- Scripts/assignment_grader.py
+++ Scripts/assignment_grader.py
@@ -7,23 +7,22 @@ import time
 import requests
 import submission
 
-
-#TODO: filter hackerrank tests (in name_id) to ensure only assignments from this semester are considered
-
 hackerrank_key = os.getenv('HACKERRANK_KEY')
 hackerrank_endpoint = os.getenv('HACKERRANK_ENDPOINT')
 #Extracts name and id of Assignment tests (eventually will also include exams)
-name_id = lambda info: {test['name'][:12]: test['id'] for test in info if 'Assignment' in test['name'].split(' ')[0]}
+name_id = lambda info: {test['name']: test['id'] for test in info if 'Assignment' in test['name'].split(' ')[0] and (['1', '2'].count(test['name'].split(' ')[1]) != 0)}
 
 #Get dictionary of test names and ids as key value pairs
 def get_tests():
     try:
         temp = name_id(requests.get(hackerrank_endpoint + 'tests?limit=20&offset=0', auth=(hackerrank_key, '')).json()['data'])
+        print('Tests: ' + str(temp))
+        return temp
     except Exception as e:
         print('Failed to get all tests.')
         print(e)
-    return temp
-
+        return None
+    
 #get dictionary of assignment names and an array of student submissions as key value pairs
 def get_students():
     test_ids = get_tests()
@@ -31,25 +30,41 @@ def get_students():
     for test in test_ids:
         student_info.update({test: []})
         try:
-            submissions = requests.get(hackerrank_endpoint + 'tests/' + test_ids[test] + '/candidates?limit=59&offset=0', auth=(hackerrank_key, '')).json()['data']
+            headers = {'authorization': 'Bearer {}'.format(hackerrank_key), 'content-type': 'application/json'}
+            submissions = requests.get(hackerrank_endpoint + 'tests/' + test_ids[test] + '/candidates?limit=59&offset=0', headers=headers).json()['data']
             for entry in submissions:
                 if entry['status'] == 7:
-                    details = entry['candidate_details']
-                    temp = submission.Submission(details[0]['value'], entry['score'], entry['plagiarism_status'], entry['pdf_url'])
+                    #score and report have been generated (entry is ready)
+                    temp = submission.Submission(get_eid(entry['candidate_details']), entry['percentage_score'], entry['plagiarism_status'], entry['pdf_url'], entry['attempt_endtime'])
                     student_info[test].append(temp)
-                    if len(details[2]['value']) > 0:
-                        student_info[test].append(submission.Submission(details[2]['value'], entry['score'], entry['plagiarism_status'], entry['pdf_url']))
+                    if len(get_partner_eid(entry['candidate_details'])) > 3:
+                        #there was a partner so add another submission obj for the partner.
+                        student_info[test].append(submission.Submission(get_partner_eid(entry['candidate_details']), entry['percentage_score'], entry['plagiarism_status'], entry['pdf_url'], entry['attempt_endtime']))
                 else:
-                    print('Code not submitted')
+                    print('Code not submitted for ' + test + ': ' + str(get_eid(entry['candidate_details'])))
         except Exception as e:
             print('Failed on ' + test + ': ' + test_ids[test])
             print(e)
     return student_info
 
+#get student eid
+def get_eid(candidate_details):
+    for detail in candidate_details:
+        if detail['title'] == 'EID':
+            return detail['value']
+    return None
+
+#get partner eid if there is one
+def get_partner_eid(candidate_details):
+    for detail in candidate_details:
+        if detail['title'] == 'Partner EID':
+            return detail['value']
+    return None
 
 if __name__ == '__main__':
     result = get_students()
     for test in result:
         for entry in result[test]:
             print(str(entry))
+    
     
--- Scripts/submission.py
+++ Scripts/submission.py
@@ -1,10 +1,14 @@
+import datetime
+
+
 class Submission():
-    def __init__(self, eid, score, plagiarism, pdf):
+    def __init__(self, eid, score, plagiarism, pdf, submit_time):
         self.eid = eid
         self.score = score
         self.plagiarism = plagiarism
         self.pdf = pdf
+        self.submit_time = datetime.datetime.strptime(submit_time,'%Y-%m-%dT%H:%M:%S+%f').date()
     
     def __str__(self):
-        return 'eid: {self.eid}, score: {self.score}, plagiarism: {self.plagiarism}, pdf_url: {self.pdf}'.format(self=self)
+        return 'eid: {self.eid}, score: {self.score}, plagiarism: {self.plagiarism}, pdf_url: {self.pdf}, submit_time: {self.submit_time}'.format(self=self)
         
